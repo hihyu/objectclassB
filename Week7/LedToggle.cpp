96be4c001ba233c6262b65ad56c60a722befb1ff
hihyu <163224230+hihyu@users.noreply.github.com>
2025-04-15 11:47:47 +0900

    Update LedToggle.cpp

--- Week7/LedToggle.cpp
+++ Week7/LedToggle.cpp
@@ -3,11 +3,25 @@
 LedToggle::LedToggle(int pin) {
 	_pin = pin;
 	_state = false;
+	//_delayTime = 0; 딜레이 추가
 	pinMode(_pin, OUTPUT);
 	digitalWrite(_pin, LOW);
 }
 
+//LedToggle::LedToggle(int pin, unsigned long delayTime) { 딜레이 추가
+//	_pin = pin;
+//	_state = false;
+//	_delayTime = delayTime;
+//	pinMode(_pin, OUTPUT);
+//	digitalWrite(_pin, LOW);
+//}
+
 void LedToggle::toggle() {
+	
+	//if(_delayTime > 0) { 딜레이 추가
+	//	delay(_delayTime);
+	//}
+	
 	_state = !_state;
 	digitalWrite(_pin, _state ? HIGH : LOW);
 }
