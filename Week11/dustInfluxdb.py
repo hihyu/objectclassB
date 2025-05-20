c47da830276540b238940564cd9cbac20d49f661
hihyu <163224230+hihyu@users.noreply.github.com>
2025-05-20 15:54:14 +0900

    Create dustInfluxdb.py

---
+++ Week11/dustInfluxdb.py
@@ -0,0 +1,37 @@
+import time
+import requests, json
+from influxdb import InfluxDBClient as influxdb
+import serial
+
+seri = serial.Serial('/dev/ttyACM0', baudrate = 9600, timeout = None)
+
+
+while(True)
+  time.sleep(1)
+  if seri.in_waiting != 0:
+    content = seri.readline()
+    a = float(content.decode())
+    data = [{
+        'measurement' : 'dust',
+        'tags':{
+            'InhaUni' : '2222',
+        },
+        'fieds':{
+            'dust' : a,
+        }
+    }]
+    clinet = None
+    try:
+        client = influxdb('localhost',8086,'root','dust')
+    except Exception as e:
+      print("Exception" + str(e))
+    if client is not None:
+        try:
+            client.write_points(data)
+        except Exception as e:
+            print("Exception write" + str(e))
+        finally:
+            client.close()
+      print(a)
+      print("running influxdb OK")
+          
